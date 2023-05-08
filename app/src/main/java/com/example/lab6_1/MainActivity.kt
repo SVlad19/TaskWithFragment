4b0322a6570a39161790d4c044c218a03dd7a3f8
SVlad19 <mr.vladik.semeno3@gmail.com>
2023-05-08 23:44:29 +0300

    Added Fragment

--- app/src/main/java/com/example/lab6_1/MainActivity.kt
+++ app/src/main/java/com/example/lab6_1/MainActivity.kt
@@ -2,10 +2,117 @@ package com.example.lab6_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.lab6_1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnFirst.setOnClickListener(){
+            val fragment = MultiplicationTable.newInstance("1")
+
+            val transaction = supportFragmentManager.beginTransaction()
+
+            transaction.replace(R.id.flCont, fragment)
+
+            transaction.commit()
+        }
+
+        binding.btnSecond.setOnClickListener(){
+            val fragment = MultiplicationTable.newInstance("2")
+
+            val transaction = supportFragmentManager.beginTransaction()
+
+            transaction.replace(R.id.flCont, fragment)
+
+            transaction.commit()
+        }
+
+        binding.btnThird.setOnClickListener(){
+            val fragment = MultiplicationTable.newInstance("3")
+
+            val transaction = supportFragmentManager.beginTransaction()
+
+            transaction.replace(R.id.flCont, fragment)
+
+            transaction.commit()
+        }
+
+        binding.btnFourth.setOnClickListener(){
+            val fragment = MultiplicationTable.newInstance("4")
+
+            val transaction = supportFragmentManager.beginTransaction()
+
+            transaction.replace(R.id.flCont, fragment)
+
+            transaction.commit()
+        }
+
+        binding.btnFifth.setOnClickListener(){
+            val fragment = MultiplicationTable.newInstance("5")
+
+            val transaction = supportFragmentManager.beginTransaction()
+
+            transaction.replace(R.id.flCont, fragment)
+
+            transaction.commit()
+        }
+
+        binding.btnSixth.setOnClickListener(){
+            val fragment = MultiplicationTable.newInstance("6")
+
+            val transaction = supportFragmentManager.beginTransaction()
+
+            transaction.replace(R.id.flCont, fragment)
+
+            transaction.commit()
+        }
+
+        binding.btnSeventh.setOnClickListener(){
+            val fragment = MultiplicationTable.newInstance("7")
+
+            val transaction = supportFragmentManager.beginTransaction()
+
+            transaction.replace(R.id.flCont, fragment)
+
+            transaction.commit()
+        }
+
+        binding.btnEighth.setOnClickListener(){
+            val fragment = MultiplicationTable.newInstance("8")
+
+            val transaction = supportFragmentManager.beginTransaction()
+
+            transaction.replace(R.id.flCont, fragment)
+
+            transaction.commit()
+        }
+
+        binding.btnNinth.setOnClickListener(){
+            val fragment = MultiplicationTable.newInstance("9")
+
+            val transaction = supportFragmentManager.beginTransaction()
+
+            transaction.replace(R.id.flCont, fragment)
+
+            transaction.commit()
+        }
+
+        binding.btnTenth.setOnClickListener(){
+            val fragment = MultiplicationTable.newInstance("10")
+
+            val transaction = supportFragmentManager.beginTransaction()
+
+            transaction.replace(R.id.flCont, fragment)
+
+            transaction.commit()
+        }
+
+
+
     }
 }
--- app/src/main/java/com/example/lab6_1/MultiplicationTable.kt
+++ app/src/main/java/com/example/lab6_1/MultiplicationTable.kt
@@ -5,11 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentActivity
+import com.example.lab6_1.databinding.FragmentMultiplicationTableBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -17,15 +18,14 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class MultiplicationTable : Fragment() {
-    // TODO: Rename and change types of parameters
+    private lateinit var binding: FragmentMultiplicationTableBinding
     private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
@@ -33,32 +33,47 @@ class MultiplicationTable : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_multiplication_table, container, false)
+        binding = FragmentMultiplicationTableBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        fillText()
 
     }
 
+    fun fillText(){
+        binding.tvEl1.text = param1
+        binding.tvEl2.text = param1
+        binding.tvEl3.text = param1
+        binding.tvEl4.text = param1
+        binding.tvEl5.text = param1
+        binding.tvEl6.text = param1
+        binding.tvEl7.text = param1
+        binding.tvEl8.text = param1
+        binding.tvEl9.text = param1
+        binding.tvEl10.text = param1
+
+        binding.tvRes1.text= (1 * (binding.tvEl1.text.toString().toInt())).toString()
+        binding.tvRes2.text= (2 * (binding.tvEl2.text.toString().toInt())).toString()
+        binding.tvRes3.text= (3 * (binding.tvEl3.text.toString().toInt())).toString()
+        binding.tvRes4.text= (4 * (binding.tvEl4.text.toString().toInt())).toString()
+        binding.tvRes5.text= (5 * (binding.tvEl5.text.toString().toInt())).toString()
+        binding.tvRes6.text= (6 * (binding.tvEl6.text.toString().toInt())).toString()
+        binding.tvRes7.text= (7 * (binding.tvEl7.text.toString().toInt())).toString()
+        binding.tvRes8.text= (8 * (binding.tvEl8.text.toString().toInt())).toString()
+        binding.tvRes9.text= (9 * (binding.tvEl9.text.toString().toInt())).toString()
+        binding.tvRes10.text= (10 * (binding.tvEl10.text.toString().toInt())).toString()
+    }
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MultiplicationTable.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(param1: String) =
             MultiplicationTable().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
                 }
             }
     }
